83814c5cf5dead76e9dba3bf4ae4ebac98cf3098
DESKTOP-C1DP7AI\AomsinBOY <oomsinboyyyy@gmail.com>
2022-10-13 01:35:59 +0700

    แก้box

--- lib/homepage/page_1home.dart
+++ lib/homepage/page_1home.dart
@@ -15,89 +15,89 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffE7ECFF),
       body: SafeArea(
           child: Center(
         child: SingleChildScrollView(
             child: Column(
           children: [
-            Material(
-              borderRadius: BorderRadius.circular(25),
-              color: Color(0xff2F3A61),
-              child: MaterialButton(
-                padding: EdgeInsets.fromLTRB(80, 20, 80, 20),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Add_drug()));
-                },
-                child: Text(
-                  "+เพิ่มยา ",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontWeight: FontWeight.bold),
-                ),
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Add_drug()));
+              },
+              icon: Icon(Icons.medical_information),
+              label: Text(
+                "เพิ่มยา",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontWeight: FontWeight.bold),
               ),
+              style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 102),
+                  backgroundColor: Color(0xff804F4F),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25))),
             ),
             SizedBox(height: 40),
-            Material(
-              borderRadius: BorderRadius.circular(25),
-              color: Color(0xff2F3A61),
-              child: MaterialButton(
-                padding: EdgeInsets.fromLTRB(80, 20, 80, 20),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MyHome()));
-                },
-                child: Text(
-                  "รายการยา",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontWeight: FontWeight.bold),
-                ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.assignment),
+              label: Text(
+                "รายการยา",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontWeight: FontWeight.bold),
               ),
+              style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 76),
+                  backgroundColor: Color(0xff804F4F),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25))),
             ),
             SizedBox(height: 40),
-            Material(
-              borderRadius: BorderRadius.circular(25),
-              color: Color(0xff2F3A61),
-              child: MaterialButton(
-                padding: EdgeInsets.fromLTRB(80, 20, 80, 20),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MyHome()));
-                },
-                child: Text(
-                  "ประวัติยา",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontWeight: FontWeight.bold),
-                ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.medication_liquid),
+              label: Text(
+                "ประวัติยา",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontWeight: FontWeight.bold),
               ),
+              style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 84),
+                  backgroundColor: Color(0xff804F4F),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25))),
             ),
             SizedBox(height: 40),
-            Material(
-              borderRadius: BorderRadius.circular(25),
-              color: Color(0xff2F3A61),
-              child: MaterialButton(
-                padding: EdgeInsets.fromLTRB(80, 20, 80, 20),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MyHome()));
-                },
-                child: Text(
-                  "ข้อมูลส่วนตัว",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontWeight: FontWeight.bold),
-                ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.account_circle),
+              label: Text(
+                "ข้อมูลส่วนตัว",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontWeight: FontWeight.bold),
               ),
+              style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 60),
+                  backgroundColor: Color(0xff804F4F),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25))),
             ),
             SizedBox(height: 40),
           ],
